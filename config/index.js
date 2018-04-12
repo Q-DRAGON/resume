f91b4b46f6c8468d0e3a4a6377ea1065ea6bbda5
Christina <z574203238@outlook.com>
2018-04-13 00:22:36 +0800

    edit

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/resume/dist',
+    assetsPublicPath: '/resume/dist/',
 
     /**
      * Source Maps
